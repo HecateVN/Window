0c1f8929ea6a9428cf97be00987e9580c2dd6a00
HecateVN <hieubmk456@gmail.com>
2024-03-11 00:26:26 +0700

    Add project files.

---
+++ test/Form_LogOut.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form_LogOut : Form
+    {
+        public Form_LogOut()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_LogOut_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+        }
+    }
+}
---
+++ test/Form_User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form_User : Form
+    {
+        public Form_User()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_User_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+        }
+    }
+}
